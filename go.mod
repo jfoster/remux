577269040906ebe74b3f931c244dd7aacca82d28
jfoster <jacobfoster@pm.me>
2020-02-17 10:28:07 +0000

    Use goffpeg for transcoding

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jfoster/mkv2mp4
 
 go 1.13
 
-require github.com/radovskyb/watcher v1.0.7
+require (
+	github.com/jfoster/goffmpeg v0.0.0-20200217101643-125ae7f72136
+	github.com/radovskyb/watcher v1.0.7
+)
--- mkv2mp4.go
+++ mkv2mp4.go
@@ -2,10 +2,12 @@ package mkv2mp4
 
 import (
 	"errors"
-	"os/exec"
+	"fmt"
 	"path"
 	"path/filepath"
 	"strings"
+
+	"github.com/jfoster/goffmpeg/transcoder"
 )
 
 func Convert(in string) error {
@@ -14,14 +16,24 @@ func Convert(in string) error {
 	}
 	out := trimExt(in) + ".mp4"
 
-	cmd := exec.Command("ffmpeg",
-		"-y",
-		"-i", in,
-		"-map", "0",
-		"-c", "copy",
-		out,
-	)
-	return cmd.Run()
+	trans := new(transcoder.Transcoder)
+
+	if err := trans.Initialize(in, out); err != nil {
+		return err
+	}
+
+	trans.MediaFile().SetStreamMap("0")
+	trans.MediaFile().SetAudioCodec("copy")
+	trans.MediaFile().SetVideoCodec("copy")
+
+	done := trans.Run(true)
+
+	progress := trans.Output()
+	for msg := range progress {
+		fmt.Println(msg)
+	}
+
+	return <-done
 }
 
 func trimExt(p string) string {
